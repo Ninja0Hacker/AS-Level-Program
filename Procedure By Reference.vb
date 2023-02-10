b7e22a43a233546cd6d6293f75ca7e0106ece848
Sheran Sameer <103332864+Ninja0Hacker@users.noreply.github.com>
2023-02-10 19:57:52 +0400

    Create Procedure By Reference.vb

---
+++ Procedure By Reference.vb
@@ -0,0 +1,17 @@
+Module Module1
+
+    Sub Main()
+        Dim x As Integer
+        x = 4
+        Call myproc(x)
+        Console.WriteLine(x)
+        Console.ReadKey()
+    End Sub
+
+    Sub myproc(ByRef x As Integer)
+        Dim z As Integer
+        x = x + 1
+        z = z + 3
+    End Sub
+
+End Module
